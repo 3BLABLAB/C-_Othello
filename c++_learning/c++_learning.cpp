ad3f892c350aad4f4f95ac03bbc5164bd584a7dc
3BLABLAB <rk3b.0215@gmail.com>
2025-09-13 19:06:28 +0900

    Update:反転成功
    1dに置いたとき挙動不審

--- c++_learning/c++_learning.cpp
+++ c++_learning/c++_learning.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+/*
+3e
+3d
+3c
+2d
+5c
+3b
+1d
+*/
+
 //0:- , 1:● , -1:○ 
 int board[BOARD_SIZE][BOARD_SIZE] = {};
 //1:● -1:○ 
@@ -47,33 +57,66 @@ void show_player() {
 
 void input_check(char* x, char* y) {
 	//xが英字 yが数字になるようにする
-	if (*x >= 1 && *x <= 9) {
-		swap(x, y);
+	if (*x >= '1' && *x <= '9') {
+		swap(*x, *y);
 	}
 	else {
 		return;
 	}
 }
 
+bool inside_board(int i, int j) {
+	if (i < 0 || i >= BOARD_SIZE) {
+		cout << "outside" << endl;
+		return false;
+	}
+	else if (j < 0 || j >= BOARD_SIZE) {
+		cout << "outside" << endl;
+		return false;
+	}
+	return true;
+}
+
 bool check_plc(int i, int j) {
 	//盤面の範囲内かどうか
-	if (i < 1 || i>9 || j < 1 || j>9)return false;
+	if (!inside_board(i,j)) {
+		return false;
+	}
+
+	//有効なマスか
+	if (board[i][j] != 0) {
+		return false;
+	}
 
 	//相手の駒を挟めるか
 	for (int d_i = -1; d_i < 2; d_i++) {
 		for (int d_j = -1; d_j < 2; d_j++) {
+			if (d_i == 0 && d_j == 0) continue;
 			//相手の駒が何個続くか
 			int times = 1;
-			//注目するマスが有効範囲内か
-			if (i + d_i * times < 1 || i + d_i * times>9)break;
-			if (j + d_j * times < 1 || j + d_j * times>9)break;
 
-			//注目するマスが相手の駒だったら
-			while (board[i + d_i*times][j + d_j*times] == player * -1) {
+			//相手の駒が続いているか
+			while (true) {
+				int next_i = i + d_i * times;
+				int next_j = j + d_j * times;
+
+				//注目するマスが有効範囲内か
+				if (!inside_board(next_i, next_j))break;
+				//注目するマスが相手の駒じゃなかったら
+				if (board[next_i][next_j] != player * -1) {
+					break;
+				}
 				times++;
 			}
+
+			int next_i = i + d_i * times;
+			int next_j = j + d_j * times;
+			cout << "debug check: i=" <<next_i<<" j:"<<next_j<< endl;
+			//注目するマスが有効範囲内か
+			if (!inside_board(next_i, next_j))break;
+			//cout << "debug check" << endl;
 			//自分の駒で挟めていたら
-			if (board[i + d_i * times][j + d_j * times] == player) {
+			if (board[next_i][next_j] == player && times>1) {
 				return true;
 			}
 		}
@@ -87,19 +130,32 @@ void place_stn(int i, int j) {
 	//相手の駒を挟めるか
 	for (int d_i = -1; d_i < 2; d_i++) {
 		for (int d_j = -1; d_j < 2; d_j++) {
+			if (d_i == 0 && d_j == 0) continue;
 			//相手の駒が何個続くか
 			int times = 1;
-			//注目するマスが有効範囲内か
-			if (i + d_i * times < 1 || i + d_i * times>9)break;
-			if (j + d_j * times < 1 || j + d_j * times>9)break;
 
-			//注目するマスが相手の駒だったら
-			while (board[i + d_i * times][j + d_j * times] == player * -1) {
+			//相手の駒が続いているか
+			while (true) {
+				int next_i = i + d_i * times;
+				int next_j = j + d_j * times;
+
+				//注目するマスが有効範囲内か
+				if (!inside_board(next_i, next_j))break;
+				//注目するマスが相手の駒じゃなかったら
+				if (board[next_i][next_j] != player * -1) {
+					break;
+				}
 				times++;
 			}
+
+			int next_i = i + d_i * times;
+			int next_j = j + d_j * times;
+			//注目するマスが有効範囲内か
+			if (!inside_board(next_i, next_j))break;
 			//自分の駒で挟めていたら
-			if (board[i + d_i * times][j + d_j * times] == player) {
+			if (board[next_i][next_j] == player && times > 1) {
 				rep(k, times) {
+					//駒を裏返していく
 					board[i + d_i * k][j + d_j * k] = player;
 				}
 			}
@@ -112,7 +168,9 @@ bool flag_fin() {
 	rep(i, BOARD_SIZE) {
 		rep(j, BOARD_SIZE) {
 			//どこかに置ける
-			if (check_plc(i, j))return true;
+			if (check_plc(i, j)) {
+				return true;
+			}
 		}
 	}
 
@@ -121,12 +179,16 @@ bool flag_fin() {
 	rep(i, BOARD_SIZE) {
 		rep(j, BOARD_SIZE) {
 			//どこかに置ける
-			{
+			if (check_plc(i, j)) {
 				cout << "パスしました" << endl;
-				if (check_plc(i, j))return true;
+				player *= -1;
+				return true;
 			}
 		}
 	}
+	cout << "どこにも置けません" << endl;
+	if (player == 1)cout << "後手の勝利です" << endl;
+	else if (player == -1)cout << "先手の勝利です" << endl;
 	return false;
 }
 
@@ -148,6 +210,7 @@ void judge() {
 int main() {
 	make_board();
 	char a = 'a';
+	int turn = 0;
 	while (flag_fin()) {
 		show_board();
 		show_player();
@@ -163,9 +226,12 @@ int main() {
 			x = tx - 'a';
 			y = ty - '1';
 		} while (!check_plc(x, y));
-		cout << x << "行" << y << "列目" << endl;
+		//cout << x << "行" << y << "列目" << endl
+		//cout << "player:" << player << endl;
+		cout << "place" << endl;
 		place_stn(x, y);
 		player *= -1;
+		cout << "turn:" << ++turn << endl;
 	}
 	show_board();
 	judge();
